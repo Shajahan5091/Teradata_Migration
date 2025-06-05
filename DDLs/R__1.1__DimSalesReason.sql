d072cef4890a61ce1809a26f5abce075e15a9ef4
Shajahan5091 <shajahanshajahan5091@gmail.com>
2025-06-05 07:34:36 -0400

    Add converted DDL files with prefix

---
+++ DDLs/R__1.1__DimSalesReason.sql
@@ -0,0 +1,13 @@
+﻿/* <sc-table> AdventureWorksDW.DimSalesReason </sc-table> */
+--** SSC-FDM-TD0024 - SET TABLE FUNCTIONALITY NOT SUPPORTED. TABLE MIGHT HAVE DUPLICATE ROWS **
+CREATE OR REPLACE TABLE ADVENTUREWORKSDW.SNOWCONVERT.DimSalesReason
+(
+     SalesReasonKey INTEGER NOT NULL,
+     SalesReasonAlternateKey INTEGER NOT NULL,
+     SalesReasonName VARCHAR(50) NOT NULL,
+     SalesReasonReasonType VARCHAR(50) NOT NULL,
+PRIMARY KEY ( SalesReasonKey ))
+COMMENT = '{ "origin": "sf_sc", "name": "snowconvert", "version": {  "major": 1,  "minor": 7,  "patch": "0.0" }, "attributes": {  "component": "teradata",  "convertedOn": "06/05/2025",  "domain": "no-domain-provided" }}'
+;
+
+--
