cde2cd832cf704f27389c9c345faf2e2de9ca22a
Shajahan5091 <shajahanshajahan5091@gmail.com>
2025-08-14 00:36:38 -0400

    Add original Teradata DDLs and converted Snowflake DDLs with CREATE IF NOT EXISTS and schema change prefixes

---
+++ DDLs/Teradata_DDLs/AdventureWorksDW/Snowconvert/Functions/DDL_Functions.sql
@@ -0,0 +1,34 @@
+﻿--
+/* <sc-function> AdventureWorksDW.ufn_CalculateDiscountedAmount </sc-function> */
+CREATE FUNCTION AdventureWorksDW.ufn_CalculateDiscountedAmount(amount DECIMAL(18,2), discount_percent DECIMAL(5,2)
+)
+RETURNS DECIMAL(18,2)
+LANGUAGE SQL 
+DETERMINISTIC 
+CONTAINS SQL
+SQL SECURITY DEFINER
+COLLATION INVOKER
+INLINE TYPE 1
+RETURN amount * (1 - (discount_percent / 100));
+
+--
+/* <sc-function> AdventureWorksDW.ufn_QuarterName </sc-function> */
+CREATE FUNCTION AdventureWorksDW.ufn_QuarterName (
+	quarter_num BYTEINT
+)
+RETURNS VARCHAR(10)
+LANGUAGE SQL 
+DETERMINISTIC 
+CONTAINS SQL
+SQL SECURITY DEFINER
+COLLATION INVOKER
+INLINE TYPE 1
+RETURN
+	CASE quarter_num
+		WHEN 1 THEN 'Q1'
+		WHEN 2 THEN 'Q2'
+		WHEN 3 THEN 'Q3'
+		WHEN 4 THEN 'Q4'
+		ELSE 'Invalid'
+	END;
+
